50db3bfdecd0090a4091f4f1eb2005bbee229375
atakan <atakan.unlu48@gmail.com>
2026-01-09 01:54:23 +0300

    Extra stream

--- src/extra_Lambda/LambdaAndStreamAPI.java
+++ src/extra_Lambda/LambdaAndStreamAPI.java
@@ -1,5 +1,10 @@
 package extra_Lambda;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class LambdaAndStreamAPI {
     public static void main(String[] args) {
 
@@ -13,24 +18,46 @@ public class LambdaAndStreamAPI {
 //            }
 //        };
 
-        Walkable obj = (steps,isEnabled) -> {
-            System.out.println(" walking fast + " + steps +" steps" + isEnabled);
-            return 2*steps;
+//        Walkable obj = (steps,isEnabled) -> {
+//            System.out.println(" walking fast + " + steps +" steps" + isEnabled);
+//            return 2*steps;
+//
+//        };
+//
+//        Walkable obj2 = (steps, isEnabled) -> 2*steps;
+//
+//        obj.walk(8,true);
 
-        };
 
-        Walkable obj2 = (steps, isEnabled) -> 2*steps;
+        List<String> fruits = List.of("Apple", "Kiwi","Banana");
 
-        obj.walk(8,true);
+        Map<String,Integer> fruitList = fruits.stream()
+               // .map(fruit -> fruit.length())
+                .collect(Collectors.toMap(
+                        fruit -> fruit,
+                        String::length
+                ));
+
+        System.out.println(fruitList);
+
+       Stream<String> stream = fruits.stream();
+
+//
+        stream
+                .filter(fruit -> fruit.length() < 12)
+                .sorted()
+                .map(String::length)
+                .map(fruit -> 2*fruit)
+                .forEach(System.out::println);
 
     }
 
 }
 
 
-interface Walkable{
-    int walk(int steps,boolean isEnabled);
-}
+//interface Walkable{
+//    int walk(int steps,boolean isEnabled);
+//}
 
 //class WalkFast implements Walkable{
 //
